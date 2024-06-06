8cf8019964cddb6e5c0cd291f9202415e9224868
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-06-07 01:44:49 +0530

    home page

--- app/page.js
+++ app/page.js
@@ -2,8 +2,60 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      HOMEPAGE
-    </div>
+    <>
+      <div className="flex justify-center flex-col items-center text-white h-[44vh] gap-4">
+        <div className="font-bold flex gap-2 text-5xl">By Me a Chai!
+          <span><img width={70} src="/chai.gif" alt="" /></span></div>
+        <p>A Crowdfunding platform for craetors. Get funded by your fans and followers. Start Now!</p>
+        <div>
+          <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
+          <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
+        </div>
+      </div>
+      <div className="bg-white h-1 opacity-15">
+      </div>
+      <div className="text-white container mx-auto py-16">
+        <h1 className="text-2xl font-bold text-center my-10">Your Fans can buy you a Chai</h1>
+        <div className="flex gap-5 justify-around">
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/maal.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/mall.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/b.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Your fans are available for you to help you</p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white h-1 opacity-15">
+      </div>
+      <div className="text-white container mx-auto py-16">
+        <h1 className="text-2xl font-bold text-center my-10">Your Fans can buy you a Chai</h1>
+        <div className="flex gap-5 justify-around">
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/maal.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/mall.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/b.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Your fans are available for you to help you</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
